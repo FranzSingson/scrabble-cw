dbef20f67583b9b85225a002170c8585fde86b5b
franz-up2011325 <74835413+franz-up2011325@users.noreply.github.com>
2021-03-04 12:46:58 +0000

    Box now only accepts 1 tile

--- script.js
+++ script.js
@@ -33,50 +33,24 @@
 let newPlayerLetters = [];
 
 function makeRandomLetter() {
-
-    const letters = [
-        letterA = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'],
-        letterB = ['B', 'B'],
-        letterC = ['C', 'C'],
-        letterD = ['D', 'D', 'D', 'D'],
-        letterE = ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
-        letterF = ['F', 'F'],
-        letterG = ['G', 'G', 'G'],
-        letterH = ['H', 'H'],
-        letterI = ['I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I'],
-        letterJ = ['J'],
-        letterK = ['K'],
-        letterL = ['L', 'L', 'L', 'L'],
-        letterM = ['M', 'M'],
-        letterN = ['N', 'N', 'N', 'N', 'N'],
-        letterO = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-        letterP = ['P', 'P'],
-        letterQ = ['Q'],
-        letterR = ['R', 'R', 'R', 'R', 'R', 'R'],
-        letterS = ['S', 'S', 'S', 'S'],
-        letterT = ['T', 'T', 'T', 'T', 'T', 'T'],
-        letterU = ['U', 'U', 'U', 'U',],
-        letterV = ['V', 'V'],
-        letterW = ['W', 'W'],
-        letterX = ['X'],
-        letterY = ['Y', 'Y'],
-        letterZ = ['Z']
-    ];
-
-    for (i = 1; i < 16; i += 1) {
-        newPlayerLetters.push(letters[Math.floor(1 + Math.random() * letters.length - 1)][0]);
+    const letters = ["S", "A", "L", "R", "S", "O", "T", "E", "L", "A", "R", "A", "D", "I", "I", "D", "Y", "O", "I", "I",
+        "M", "Z", "C", "A", "I", "W", "T", "O", "G", "Y", "G", "U", "M", "V", "X", "P", "E", "I", "E", "U", "A", "N", "E", "P", "R",
+        "J", "O", "E", "S", "A", "H", "S", " ", "Q", "N", "O", "A", "W", "E", "D", "O", "D", "F", "I", "N", "N", "U", "B", "E",
+        "T", "G", "T", "I", "V", "R", "T", "A", "R", "E", "I", "N", "A", "L", " ", "N", "O", "U", "R", "C", "O", "E", "L", "E",
+        "E", "E", "H", "T", "F", "K", "B"];
+    for (i = 0; i < 100; i += 1) {
+        newPlayerLetters.push(letters[i]);
     }
-    // console.log(newPlayerLetters);
 }
 makeRandomLetter();
-
-
+console.log(newPlayerLetters);
 
 
 function makeBoard() {
     for (let i = 1; i < 226; i += 1) {
         const newSection = document.createElement("section");
         newSection.id = `dropzone-box${i}`;
+        newSection.className = "all-boxes";
 
         const elemMain = document.getElementById("main1");
         elemMain.appendChild(newSection);
@@ -90,6 +64,7 @@ function makePlayerRack() {
     for (let i = 1; i < 8; i += 1) {
         const makeRack = document.createElement("section");
         makeRack.id = `player-rack${i}`;
+        makeRack.className = "class-player-rack"
 
         const elemMain2 = document.getElementById("main2");
         elemMain2.appendChild(makeRack);
@@ -102,9 +77,11 @@ makePlayerRack();
 function makeLetterTile() {
     for (i = 1; i < 8; i += 1) {
         const makeTile = document.createElement("div");
-        makeTile.id = "letter-tile";
+        makeTile.id = `letter-tile${i}`;
+        makeTile.className = "letter-tile";
         makeTile.draggable = true;
 
+        // const node = document.createTextNode(`${newPlayerLetters[newPlayerLetters.length - i]}`);
         const node = document.createTextNode(`${newPlayerLetters[i]}`);
         makeTile.appendChild(node);
 
@@ -113,6 +90,18 @@ function makeLetterTile() {
     }
 }
 makeLetterTile();
+
+function insertOnEmpty() {
+    const rack = document.querySelectorAll(".class-player-rack");
+    for (const rac of rack) {
+        if (rack.children < 1) {
+            makeLetterTile();
+        }
+    }
+}
+insertOnEmpty();
+
+
 
 
 
@@ -134,18 +123,6 @@ makeLetterTile();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // EventHandlers
 function dragStartHandler(e) {
     const data = e.target.id;
@@ -157,35 +134,57 @@ function dragOverHandler(e) {
 }
 
 function dropHandler(e) {
-    const data = e.dataTransfer.getData('text/plain'); //We ask for data transfer and assign it as 'data"
-    const dragged = document.getElementById(data);  //Once we have "data", we use getElementById
-    e.currentTarget.append(dragged); //We append it to the drop zone which is the source of the drop event
-}   
+
+    if (e.currentTarget.children.length < 1) {
+        const data = e.dataTransfer.getData('text/plain'); //We ask for data transfer and assign it as 'data"
+        const dragged = document.getElementById(data);  //Once we have "data", we use getElementById
+
+        e.currentTarget.append(dragged); //We append it to the drop zone which is the source of the drop event
+    };
+}
 
 
 // Handler for the dropzone on the board
-for (let i = 1; i < 226; i += 1) {
-    const boardDropzone = document.querySelectorAll(`#dropzone-box${i}`);   //A handler for all dropzones
-    for (const dropzone1 of boardDropzone) {
-        dropzone1.addEventListener('dragover', dragOverHandler);
-        dropzone1.addEventListener('drop', dropHandler);
+function boardHandler() {
+    for (let i = 1; i < 226; i += 1) {
+        const boardDropzone = document.querySelectorAll(`#dropzone-box${i}`);   //A handler for all dropzones
+        for (const dropzone1 of boardDropzone) {
+            dropzone1.addEventListener('dragover', dragOverHandler);
+            dropzone1.addEventListener('drop', dropHandler);
+        }
     }
 }
 
 // Handlers for the dropzone on the player rack
-for (i = 1; i < 8; i += 1) {
-    const playerRackDropzone = document.querySelectorAll(`#player-rack${i}`);   //Makes the rack a dropzone
-    for (const dropzone2 of playerRackDropzone) {
-        dropzone2.addEventListener('dragover', dragOverHandler);
-        dropzone2.addEventListener('drop', dropHandler);
+function playerRackHandler() {
+    for (i = 1; i < 8; i += 1) {
+        const playerRackDropzone = document.querySelectorAll(`#player-rack${i}`);   //Makes the rack a dropzone
+        for (const dropzone2 of playerRackDropzone) {
+            dropzone2.addEventListener('dragover', dragOverHandler);
+            dropzone2.addEventListener('drop', dropHandler);
+        }
     }
 }
 
-
-const divTiles = document.querySelectorAll("div");      //A handler for all our div element
-for (const div of divTiles) {
-    div.addEventListener('dragstart', dragStartHandler);    //An event listen for every single div element
+function letterTileHandler() {
+    const divTiles = document.querySelectorAll("div");      //A handler for all our div element
+    for (const div of divTiles) {
+        div.addEventListener('dragstart', dragStartHandler);    //An event listen for every single div element
+    }
 }
+
+function allHandlers() {
+    boardHandler();
+    playerRackHandler();
+    letterTileHandler();
+}
+
+allHandlers();
+
+
+
+
+
 
 
 
@@ -204,6 +203,7 @@ function set3W() {
     const box3Ws = document.querySelectorAll("#dropzone-box1, #dropzone-box8, #dropzone-box15, #dropzone-box106, #dropzone-box120, #dropzone-box211, #dropzone-box218, #dropzone-box225");
     for (const box3W of box3Ws) {
         const node = document.createTextNode("3W");
+        box3W.classList.add("dropzone3W");
         box3W.appendChild(node);
     }
 }
@@ -214,6 +214,7 @@ function set3L() {
     const box3Ls = document.querySelectorAll("#dropzone-box21, #dropzone-box25, #dropzone-box77, #dropzone-box81, #dropzone-box85, #dropzone-box89,#dropzone-box137, #dropzone-box141, #dropzone-box145, #dropzone-box149, #dropzone-box201, #dropzone-box205");
     for (const box3L of box3Ls) {
         const node = document.createTextNode("3L");
+        box3L.classList.add("dropzone3L");
         box3L.appendChild(node);
     }
 }
@@ -224,6 +225,7 @@ function set2L() {
     const box2Ls = document.querySelectorAll("#dropzone-box4, #dropzone-box12, #dropzone-box37, #dropzone-box39, #dropzone-box46, #dropzone-box53, #dropzone-box60, #dropzone-box93, #dropzone-box97, #dropzone-box99, #dropzone-box103, #dropzone-box109, #dropzone-box117, #dropzone-box123, #dropzone-box127, #dropzone-box129, #dropzone-box133, #dropzone-box166, #dropzone-box173, #dropzone-box180,#dropzone-box187, #dropzone-box189");
     for (const box2L of box2Ls) {
         const node = document.createTextNode("2L");
+        box2L.classList.add("dropzone2L");
         box2L.appendChild(node);
     }
 }
@@ -231,10 +233,10 @@ set2L();
 
 
 function set2W() {
-
     const box2Ws = document.querySelectorAll("#dropzone-box17, #dropzone-box29, #dropzone-box33, #dropzone-box43, #dropzone-box49, #dropzone-box57, #dropzone-box65, #dropzone-box71,#dropzone-box155, #dropzone-box161, #dropzone-box169, #dropzone-box177, #dropzone-box183, #dropzone-box193, #dropzone-box197, #dropzone-box209");
     for (const box2W of box2Ws) {
         const node = document.createTextNode("2W");
+        box2W.classList.add("dropzone2W");
         box2W.appendChild(node);
     }
 }
